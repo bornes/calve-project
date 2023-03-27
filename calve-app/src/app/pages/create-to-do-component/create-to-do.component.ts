13c6d3c333b5d4e9c8ce7e7a004b6113adedff6e
Sergio Teixeira <Sergio.TA.Teixeira@ctw.bmwgroup.com>
2023-03-27 15:52:26 +0100

    added input component

--- calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
+++ calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { InputComponent } from "../../components/input/input.component";
 
 @Component({
-  selector: 'app-create-to-do',
-  templateUrl: './create-to-do.component.html',
-  styleUrls: ['./create-to-do.component.scss'],
-  standalone: true,
-  imports: [CommonModule, CardComponent],
+    selector: 'app-create-to-do',
+    templateUrl: './create-to-do.component.html',
+    styleUrls: ['./create-to-do.component.scss'],
+    standalone: true,
+    imports: [CommonModule, CardComponent, InputComponent]
 })
 export class CreateToDoComponent {
-  title = "To Do List"
+  title = "To Do List";
+
+  inputValue = "";
+
+  onClick() {
+    console.log("Input value: ", this.inputValue);
+  }
 }
