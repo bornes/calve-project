e280f484150e78fa9afca1d0c479e7b4edca880c
Sergio Teixeira <Sergio.TA.Teixeira@ctw.bmwgroup.com>
2023-03-27 14:06:50 +0100

    added to do component

--- calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
+++ calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-create-to-do',
   templateUrl: './create-to-do.component.html',
   styleUrls: ['./create-to-do.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
 })
 export class CreateToDoComponent {
-
+  title = "To Do List"
 }
