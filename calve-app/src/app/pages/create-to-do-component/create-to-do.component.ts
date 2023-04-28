0853a11caa95aeacb0761b1d16ddcb4c0170faa4
Sergio Teixeira <Sergio.TA.Teixeira@ctw.bmwgroup.com>
2023-04-28 15:43:51 +0100

    added resolve button

--- calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
+++ calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
@@ -20,6 +20,8 @@ export class CreateToDoComponent {
 
   inputValue = "";
 
+  resolvedTrackList: number[] = [];
+ 
   addTrack() {
     /* The push method inserts the inputed value in the trackList's array and the String trim method removes whitespaces 
     from the beginning and end of the string and returns a new string, without modifying the original string */
@@ -27,5 +29,35 @@ export class CreateToDoComponent {
 
     // Clears the input after the click on the Add button
     this.inputValue = '';
+  }
+
+  /* To remove a track from the array, we need to use the splice method which changes the contents of an array by removing 
+  or replacing existing elements and/or adding new elements in place. The first parameter is the index at which to start 
+  changing the array and the second parameter is the number of elements to be removed, starting from the start parameter */
+  removeTrack(index: number) {
+    this.trackList.splice(index, 1);
+  }
+
+  editTrack(index: number, inputEvent: any) {
+    this.trackList[index] = inputEvent.target.textContent;
+
+    console.log(inputEvent);
+  }
+
+  resolveTrack(index: number) {
+    this.resolvedTrackList.push(index);
+
+    // let spanValue = document.getElementById('span-value');
+    // spanValue?.contentEditable = false;
+
+    // this.trackList[index] = document.getElementById('span-value')?.style.textDecoration = "line-through";
+    // contenteditable = "false;"
+
+    // spanValue?.style.textDecoration = "line-through";
+    // spanValue?.contentEditable = false;
+
+    // console.log('Span value:', spanValue);
+    console.log('Track resolved');
+    console.log(index);
   }
 }
