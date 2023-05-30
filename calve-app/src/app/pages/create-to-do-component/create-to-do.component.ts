b47a7c7956d29baf5860b54b93ea5efe49f3bf1a
Sergio Teixeira <Sergio.TA.Teixeira@ctw.bmwgroup.com>
2023-05-30 18:15:22 +0100

    added resolved list and changed parameter from index to trackId

--- calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
+++ calve-app/src/app/pages/create-to-do-component/create-to-do.component.ts
@@ -6,6 +6,13 @@ import { ButtonComponent } from "../../components/button/button.component";
 import { DropdownComponent } from "../../components/dropdown/dropdown.component";
 import { CheckboxComponent } from "../../components/checkbox/checkbox.component";
 
+type Track = {
+  trackId: string,
+  trackName: string,
+  isResolved: boolean,
+  isListened: boolean
+}
+
 @Component({
     selector: 'app-create-to-do',
     templateUrl: './create-to-do.component.html',
@@ -17,14 +24,16 @@ export class CreateToDoComponent {
 
   title = "Track List";
 
+  resolvedListTitle = 'Resolved List';
+
   // trackList = ['Track 1', 'Track 2', 'Track 3', 'Track 4'];
 
-  trackList = 
+  trackList: Track[] = 
   [
-    {trackName: 'Track 1', isResolved: false, isListened: true}, 
-    {trackName: 'Track 2', isResolved: false, isListened: false}, 
-    {trackName: 'Track 3', isResolved: false, isListened: false}, 
-    {trackName: 'Track 4', isResolved: false, isListened: false}
+    {trackId: 'track0', trackName: 'Track 1', isResolved: true, isListened: true}, 
+    {trackId: 'track1', trackName: 'Track 2', isResolved: false, isListened: false}, 
+    {trackId: 'track2', trackName: 'Track 3', isResolved: false, isListened: false}, 
+    {trackId: 'track3', trackName: 'Track 4', isResolved: false, isListened: false}
   ]
 
   inputValue = "";
@@ -39,35 +48,59 @@ export class CreateToDoComponent {
     /* The push method inserts the inputed value in the trackList's array and the String trim method removes whitespaces 
     from the beginning and end of the string and returns a new string, without modifying the original string */
     // this.trackList.push(this.inputValue.trim());
-    this.trackList.push({trackName: this.inputValue.trim(), isResolved: false, isListened: false});
+    this.trackList.push({trackId: Math.random().toString(), trackName: this.inputValue.trim(), isResolved: false, isListened: false});
 
     // Clears the input after the click on the Add button
     this.inputValue = '';
   }
 
-  /* To remove a track from the array, we need to use the splice method which changes the contents of an array by removing 
-  or replacing existing elements and/or adding new elements in place. The first parameter is the index at which to start 
-  changing the array and the second parameter is the number of elements to be removed, starting from the start parameter */
-  removeTrack(index: number) {
-    this.trackList.splice(index, 1);
+  removeTrack(trackId: string) {
+    this.trackList = this.trackList.filter(track => trackId !== track.trackId);
+
   }
 
-  editTrack(index: number, inputEvent: any) {
-    // this.trackList[index] = inputEvent.target.textContent;
-    this.trackList[index] = {...this.trackList[index], trackName: inputEvent.target.textContent}
+  // editTrack(trackId: string, inputEvent: any) {
+  //   this.trackList = this.trackList.map(track => track.trackId === trackId ? {...track, trackName: inputEvent.target.textContent} : track);
+
+  //   console.log(inputEvent);
+  // }
+
+  editTrack2(track: Track, inputEvent: any) {
+
+    track.trackName = inputEvent.target.textContent;
 
     console.log(inputEvent);
   }
 
-  resolveTrack(index: number) {
-    this.trackList[index] = {...this.trackList[index], isResolved: true, isListened: true};
+  resolveTrack(trackId: string) {
+    // this.trackList = this.trackList.map(track => {
+    // if(track.trackId === trackId) {
+    //   track.isResolved = true;
+    // }
+    //   return track;
+    // });
+
+    // A simpler approach with the map() method that creates a new array populated with the results of calling a provided function for every array element
+    this.trackList = this.trackList.map(track => (track.trackId === trackId ? {...track, isResolved: true} : track));
 
     // console.log('Span value:', spanValue);
     console.log('Track resolved');
-    console.log(index);
   }
 
-  trackListened(index: number, isListened: boolean) {
-    this.trackList[index] = {...this.trackList[index], isListened};
+  trackListened(trackId: string, isListened: boolean) {
+    // this.trackList[index] = {...this.trackList[index], isListened};
+
+    this.trackList = this.trackList.map(track => (track.trackId === trackId ? {...track, isListened} : track));
+  }
+
+  resolvedTracks() {
+    // The filter() method creates a new array filled with elements that pass a test provided by a function
+    return this.trackList.filter(track => track.isResolved);
+  }
+  
+  unresolveTrack(trackId: string) {
+    // this.trackList[index] = {...this.trackList[index], isResolved: false, isListened: false};
+
+    this.trackList = this.trackList.map(track => (track.trackId === trackId ? {...track, isResolved: false} : track));
   }
 }
